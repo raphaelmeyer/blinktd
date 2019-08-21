7b3539e918929ba9c5ba0838d8367b3a872481b3
raphael meyer <raphael.akira.meyer@gmail.com>
2019-08-21 21:14:04 +0200

    get protobuf version from protoc

--- cmake/FindProtobuf.cmake
+++ cmake/FindProtobuf.cmake
@@ -9,11 +9,36 @@ find_library(Protobuf_LIBRARY
   NAMES "protobuf"
 )
 
+find_program(Protobuf_protoc_EXECUTABLE
+  NAMES protoc
+)
+
+if(Protobuf_protoc_EXECUTABLE)
+  execute_process(
+    COMMAND ${Protobuf_protoc_EXECUTABLE} --version
+    RESULT_VARIABLE version_result
+    OUTPUT_VARIABLE version_output
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  if(version_result)
+    message(FATAL_ERROR "Could not run protoc --version")
+  else()
+    if(version_output MATCHES "libprotoc ([0-9]+\.[0-9]+\.[0-9]+)")
+      set(Protobuf_VERSION "${CMAKE_MATCH_1}")
+    else()
+      message(FATAL_ERROR "Could not get version from ${version_output}")
+    endif()
+  endif()
+else()
+  message(FATAL_ERROR "Could not find protoc")
+endif()
+
 find_package_handle_standard_args(Protobuf
   FOUND_VAR Protobuf_FOUND
   REQUIRED_VARS
     Protobuf_LIBRARY
     Protobuf_INCLUDE_DIR
+  VERSION_VAR Protobuf_VERSION
 )
 
 if(Protobuf_FOUND)
